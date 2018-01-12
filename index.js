a3244882a73d2ee465dea55ea77004169617ca6e
EnoT-SaD <s.riep@mail.ru>
2018-01-12 18:20:41 +0300

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const TelegramBot = require ('node-telegram-bot-api')
+
+
+
+const TOKEN = "507418737:AAHmDGQ5U_0oV-vnsre_Q84JjTSww8xyEwk"
+
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg =>{
+    bot.sendMessage(msg.chat.id, 'Hello from HEROKU, bot says: "Hi, ${msg.from.first_name}"')
+
+})
